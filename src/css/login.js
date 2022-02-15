ac641c4c50335b1e5e6cfb0da52a69f093130022
bhupendar007k <bhupendar@WIL-Admins-Mac-mini.local>
2022-02-15 18:10:48 +0530

    initial commit

---
+++ src/css/login.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const Section = styled.div`
+  color: #000;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background: linear-gradient(321deg, black, #5a5a5a);
+  height: 100vh;
+  /* width: 100%; */
+`;
+export const Heading = styled.h1`
+  font-family: "Outfit", sans-serif;
+  font-weight: 1000px;
+  size: 200px;
+  color: white;
+`;
+export const Para = styled.p`
+  color: red;
+  font-family: "Outfit", sans-serif;
+  size: 300px;
+`;
+export const CredentialsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space between;
+`;
+export const Input = styled.input`
+  margin: 10px 0px;
+  width: 300px;
+  height: 30px;
+  outline: none;
+  border: solid red 1px;
+  border-radius: 10px;
+`;
+export const Button = styled.button`
+  width: 300px;
+  font-size: medium;
+  height: 30px;
+  cursor: pointer;
+  color: white;
+  background: linear-gradient(296deg, #e50909, #812929);
+  margin-top: 40px;
+`;
+
+export const Container = styled.div`
+  border-radius: 50px;
+  height: 500px;
+  border: solid red 0.5 px;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 10px 20px 90px red;
+`;
---
+++ src/pages/login.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Section,
+  Para,
+  CredentialsContainer,
+  Input,
+  Button,
+  Heading,
+  Container,
+} from "../css/login";
+
+const Login = () => {
+  return (
+    <Section class="main-container">
+      <Container>
+        <Heading>Login</Heading>
+        <Para>login to virtual world.</Para>
+        <CredentialsContainer class="inner-container">
+          <Input
+            class="Inp"
+            type="text"
+            placeholder="username or Email"
+          ></Input>
+          <Input
+            class="Inp"
+            type="password"
+            placeholder="enter your password"
+          ></Input>
+        </CredentialsContainer>
+        <Button type="submit">Login</Button>
+      </Container>
+    </Section>
+  );
+};
+export default Login;
